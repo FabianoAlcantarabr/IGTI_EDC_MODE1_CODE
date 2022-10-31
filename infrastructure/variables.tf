4ccbb8c9c530236bd1659891220fb37dc717fdb0
Fabiano Alcantara <fabianoalcantarabr@yahoo.com.br>
2022-10-31 11:11:42 -0300

    infra Airflow

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -6,3 +6,15 @@ variable "lambda_function_name" {
   default = "IGTIexecutaEMRaovivo"
   
 }
+
+variable "key_pair_name" {
+  default = "fabianoalcantara-igti-teste"
+}
+
+variable "airflow_subnet_id" {
+  default = "subnet-021fc25a649924ced"
+}
+
+variable "vpc_id" {
+  default = "vpc-0f4ca55227f29d940"
+}
